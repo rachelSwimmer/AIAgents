2c5185d14b366dd7760dc589576154878e6eaa6a
Rachel Yavetz <racheli.swimmer@gmail.com>
2025-11-30 11:40:33 +0200

    Obfuscate API key in dad jokes script

--- 02_dadjokes.py
+++ 02_dadjokes.py
@@ -4,7 +4,7 @@ from agents.extensions.models.litellm_model import LitellmModel
 gemini = Agent(
     name="Assistant",
     instructions="You are a mighty creator of dad jokes",
-    model=LitellmModel(model="gemini/gemini-2.5-flash", api_key="AIzaSyCvhxmpvBGoR0L4k5flSmR_ONS1jAgAiQM")
+    model=LitellmModel(model="gemini/gemini-2.5-flash", api_key="xxxx")
 )
 
 result = Runner.run_sync(gemini, "Write a random dad joke")
